0624b27fb2309e5537f19ed47237bd71bb8c27b1
booz <jamesbooz.manumbale@g.batstate-u.edu.ph>
2024-07-30 11:12:52 +0800

    working in emc2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,10 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\URL;
+use Livewire\Livewire;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +23,26 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+
+        $appRoot = env('APP_ROOT');
+        if (!empty($appRoot)) {
+            Livewire::setUpdateRoute(fn ($handle) => Route::post(sprintf("/%s/livewire/update", $appRoot), $handle));
+        }
+
+        $appUrl = Config::get('app.url');
+        URL::forceRootUrl($appUrl);
+
+        // Check if running on localhost or 127.0.0.1
+        if (!in_array(parse_url($appUrl, PHP_URL_HOST), ['localhost', '127.0.0.1'])) {
+            if (str_contains($appUrl, 'https://')) {
+                URL::forceScheme('https');
+            } else {
+                URL::forceScheme('http');
+            }
+        }
+
+        // if (env('APP_ENV') == 'production' || 'prod') {
+        //     URL::forceScheme('https');
+        // }
     }
 }
